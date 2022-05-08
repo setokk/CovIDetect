8743a32ddf66837de7d1a73b77ea525b97843c20
setokk <tesmtracks@gmail.com>
2022-05-08 12:16:18 +0300

    Changes in EmailVerifier to support api key argument.

--- src/main/java/com/pasoftxperts/covidetect/emailverification/EmailVerifier.java
+++ src/main/java/com/pasoftxperts/covidetect/emailverification/EmailVerifier.java
@@ -1,4 +1,4 @@
-/* ------------------------------------------------------------------
+/* -------------------------------------------------------------------
 This class contains methods for verifying the email address of a user.
     Method Documentation:
         [*] boolean isValid(String email)
@@ -11,7 +11,7 @@ This class contains methods for verifying the email address of a user.
             -> Checks whether email contains one of the academic email domains.
             == Returns a string with that academic domain name.
                If it is not an academic email, returns "Not Academic".
-   ------------------------------------------------------------------ */
+   ------------------------------------------------------------------- */
 
 package com.pasoftxperts.covidetect.emailverification;
 
@@ -27,25 +27,26 @@ public class EmailVerifier
     // List of academic email domains
     public static final List<String> academicEmailList = Arrays.asList("@uom.edu.gr");
 
+    // Path in which the output result file will be written from isValid method
+    private static final String path = ".\\src\\main\\java\\emailverification\\";
+    private static final String apiKey = "65a83ee9a9dce5af8a939d3dc2dea5cd";
+
     public static boolean isValid(String email)
     {
         boolean result = false;
 
-        String path = ".\\src\\main\\java\\emailverification\\";
-
-
-        // API Email Verifier (Python JSON)
+        // Run API Email Verifier Script (Python JSON)
         try
         {
             Process process = new ProcessBuilder
-                    (path + "emailvalidator.exe", email, path)
+                    (path + "emailvalidator.exe", email, apiKey, path)
                     .start();
             process.waitFor();
         }
         catch (IOException|InterruptedException e) { System.exit(1); }
 
 
-        // Reading the output of the email verifier script
+        // Reading the output of the Email Verifier Script
         try
         {
             File resultFile = new File(path + "ValidEmailOutput.txt");
